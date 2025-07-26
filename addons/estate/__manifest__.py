2c68704a93832830609187a4d1b2d92a8ad1b549
agusharyokp <agusharyokp@gmail.com>
2025-07-26 11:12:55 +0700

    UI for estate property CRUD

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -3,6 +3,8 @@
     'version': '1.0',
     'depends': ['base'],
     'data': [
+        'views/estate_menus.xml',
+        'views/estate_property_views.xml', 
         'security/ir.model.access.csv',
     ],
     'installable': True,
