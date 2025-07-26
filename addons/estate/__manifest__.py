b9bdca5b926bf1d9a08ae333a7136531f6c47e03
agusharyokp <agusharyokp@gmail.com>
2025-07-26 23:23:40 +0700

    Model relation: create estate type, tags, and offer

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -3,8 +3,11 @@
     'version': '1.0',
     'depends': ['base'],
     'data': [
+        'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_menus.xml',
-        'views/estate_property_views.xml', 
         'security/ir.model.access.csv',
     ],
     'installable': True,
